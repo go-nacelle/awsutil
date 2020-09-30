a97ca02a223d5d925d0c85aaa5820364742c9451
Eric Fritz <eric@eric-fritz.com>
2020-09-30 15:01:01 -0500

    WIP.

--- go.mod
+++ go.mod
@@ -4,12 +4,10 @@ go 1.15
 
 require (
 	github.com/aws/aws-sdk-go v1.20.7
-	github.com/efritz/go-mockgen v0.0.0-20200929171327-791aef7671ed
+	github.com/derision-test/go-mockgen v0.0.0-20200930195455-0a3448e89b1d
+	github.com/efritz/go-mockgen v0.0.0-20200929171327-791aef7671ed // indirect
 	github.com/go-nacelle/log v1.0.1
 	github.com/go-nacelle/nacelle v1.0.1
 	github.com/google/go-cmp v0.5.2 // indirect
-	github.com/onsi/gomega v1.10.2
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/stretchr/testify v1.4.0
-	gotest.tools v2.2.0+incompatible
 )
--- logger_test.go
+++ logger_test.go
@@ -3,7 +3,7 @@ package awsutil
 import (
 	"testing"
 
-	mockassert "github.com/efritz/go-mockgen/assert"
+	mockassert "github.com/derision-test/go-mockgen/testutil/assert"
 	logmocks "github.com/go-nacelle/log/mocks"
 	"github.com/go-nacelle/nacelle/mocks"
 	"github.com/stretchr/testify/assert"
@@ -13,7 +13,7 @@ func TestLogAdapter(t *testing.T) {
 	logger := mocks.NewMockLogger()
 	adapter := NewAWSLogAdapter(logger)
 	adapter.Log("Hello")
-	mockassert.CalledOnceMatching(t, logger.DebugFunc, func(t assert.TestingT, call interface{}) bool {
+	mockassert.CalledOnceWith(t, logger.DebugFunc, func(t assert.TestingT, call interface{}) bool {
 		return call.(logmocks.LoggerDebugFuncCall).Arg0 == "Hello" // TODO - ergonomics
 	})
 }
@@ -22,7 +22,7 @@ func TestLogAdapterWithArgs(t *testing.T) {
 	logger := mocks.NewMockLogger()
 	adapter := NewAWSLogAdapter(logger)
 	adapter.Log("Hello, %s and %s", "bar", "baz")
-	mockassert.CalledOnceMatching(t, logger.DebugFunc, func(t assert.TestingT, call interface{}) bool {
+	mockassert.CalledOnceWith(t, logger.DebugFunc, func(t assert.TestingT, call interface{}) bool {
 		c := call.(logmocks.LoggerDebugFuncCall)
 		return c.Arg0 == "Hello, %s and %s" // TODO - ergonomics
 		// TODO - && c.Arg1 == "bar" && c.Arg2 == "baz"
@@ -33,7 +33,7 @@ func TestLogAdapterNonStringFormat(t *testing.T) {
 	logger := mocks.NewMockLogger()
 	adapter := NewAWSLogAdapter(logger)
 	adapter.Log([]string{"this", "is", "dangerous"})
-	mockassert.CalledOnceMatching(t, logger.DebugFunc, func(t assert.TestingT, call interface{}) bool {
+	mockassert.CalledOnceWith(t, logger.DebugFunc, func(t assert.TestingT, call interface{}) bool {
 		return call.(logmocks.LoggerDebugFuncCall).Arg0 == "[this is dangerous]" // TODO - ergonomics
 	})
 }
