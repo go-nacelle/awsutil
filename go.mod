36f819d793086b139b5f998ea93c9555c75dff30
Eric Fritz <eric@eric-fritz.com>
2020-09-30 20:20:09 -0500

    WIP.

--- go.mod
+++ go.mod
@@ -4,9 +4,8 @@ go 1.15
 
 require (
 	github.com/aws/aws-sdk-go v1.20.7
-	github.com/derision-test/go-mockgen v0.0.0-20200930200432-0881b119f997
+	github.com/derision-test/go-mockgen v0.0.0-20201001011750-eb2233de6342
 	github.com/efritz/go-mockgen v0.0.0-20200929171327-791aef7671ed // indirect
-	github.com/go-nacelle/log v1.0.1
 	github.com/go-nacelle/nacelle v1.0.1
 	github.com/google/go-cmp v0.5.2 // indirect
 	github.com/kr/pretty v0.2.1 // indirect
--- logger_test.go
+++ logger_test.go
@@ -4,38 +4,32 @@ import (
 	"testing"
 
 	mockassert "github.com/derision-test/go-mockgen/testutil/assert"
-	logmocks "github.com/go-nacelle/log/mocks"
 	"github.com/go-nacelle/nacelle/mocks"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestLogAdapter(t *testing.T) {
 	logger := mocks.NewMockLogger()
 	adapter := NewAWSLogAdapter(logger)
 	adapter.Log("Hello")
-	mockassert.CalledOnceWith(t, logger.DebugFunc, func(t assert.TestingT, call interface{}) bool {
-		return call.(logmocks.LoggerDebugFuncCall).Arg0 == "Hello" // TODO - ergonomics
-	})
+	mockassert.CalledOnceWith(t, logger.DebugFunc, mockassert.Values("Hello"))
 }
 
 func TestLogAdapterWithArgs(t *testing.T) {
 	logger := mocks.NewMockLogger()
 	adapter := NewAWSLogAdapter(logger)
 	adapter.Log("Hello, %s and %s", "bar", "baz")
-	mockassert.CalledOnceWith(t, logger.DebugFunc, func(t assert.TestingT, call interface{}) bool {
-		c := call.(logmocks.LoggerDebugFuncCall)
-		return c.Arg0 == "Hello, %s and %s" // TODO - ergonomics
-		// TODO - && c.Arg1 == "bar" && c.Arg2 == "baz"
-	})
+	mockassert.CalledOnceWith(t, logger.DebugFunc, mockassert.Values(
+		"Hello, %s and %s",
+		"bar",
+		"baz",
+	))
 }
 
 func TestLogAdapterNonStringFormat(t *testing.T) {
 	logger := mocks.NewMockLogger()
 	adapter := NewAWSLogAdapter(logger)
 	adapter.Log([]string{"this", "is", "dangerous"})
-	mockassert.CalledOnceWith(t, logger.DebugFunc, func(t assert.TestingT, call interface{}) bool {
-		return call.(logmocks.LoggerDebugFuncCall).Arg0 == "[this is dangerous]" // TODO - ergonomics
-	})
+	mockassert.CalledOnceWith(t, logger.DebugFunc, mockassert.Values("[this is dangerous]"))
 }
 
 func TestLogAdapterNoArgs(t *testing.T) {
